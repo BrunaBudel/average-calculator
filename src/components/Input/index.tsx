56e570471db3d5c611612e0a5eec973a62ae2478
BrunaBudel <brunakbudel@gmail.com>
2023-08-17 17:03:51 -0300

    added type to inputProps

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,13 +1,18 @@
 import { View, TextInput, Text } from 'react-native'
 import { styles } from './styles'
 
-export default function Input(props) {
+type InputProps = {
+  label: string
+  placeholder: string
+}
+
+export default function Input({ label, placeholder }: InputProps) {
   return (
     <View>
-      <Text style={styles.label}>{props.label}</Text>
+      <Text style={styles.label}>{label}</Text>
       <TextInput
         style={styles.input}
-        placeholder={props.placeholder}
+        placeholder={placeholder}
         placeholderTextColor="#ccd5ae"
         keyboardType="numeric"
       />
