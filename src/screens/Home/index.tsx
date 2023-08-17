5e8c14e9ca39fa790632aa2c2c992e26301c2f8c
BrunaBudel <brunakbudel@gmail.com>
2023-08-17 16:32:49 -0300

    chore: add eslint and more components to the Home component

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,13 +1,32 @@
-import { Text, View } from 'react-native';
-import { styles } from './styles';
+import { Text, View, TextInput, TouchableOpacity } from 'react-native'
+import { styles } from './styles'
+import { FontAwesomeIcon } from '@fortawesome/react-native-fontawesome'
+import { faCalculator } from '@fortawesome/free-solid-svg-icons/faCalculator'
 
-export default function Home() {
+export function Home() {
+  function handleAverageCalc() {
+    console.log('Calculando média')
+  }
   return (
     <View style={styles.container}>
-      <Text style={styles.title}>
-        Calculadora de média
-      </Text>
+      <Text style={styles.title}>Calculadora de média</Text>
       <Text style={styles.subtitle}>Insira as notas para calcular a média</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a primeira nota"
+        placeholderTextColor="#ccd5ae"
+        keyboardType="numeric"
+      />
+      <TextInput
+        style={styles.input}
+        placeholder="Digite a segunda nota"
+        placeholderTextColor="#ccd5ae"
+        keyboardType="numeric"
+      />
+      <TouchableOpacity style={styles.button} onPress={handleAverageCalc}>
+        <FontAwesomeIcon icon={faCalculator} style={styles.icon} />
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
     </View>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,19 +1,45 @@
-import { StyleSheet } from "react-native";
+import { StyleSheet } from 'react-native'
 
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor:'#283618',
-    padding:24
+    backgroundColor: '#283618',
+    padding: 24,
   },
   title: {
     color: '#fefae0',
     fontSize: 24,
     fontWeight: 'bold',
-    marginTop: 48
+    marginTop: 48,
   },
   subtitle: {
     color: '#faedcd',
-    fontSize: 16
-  }
-});
+    fontSize: 16,
+  },
+  input: {
+    height: 45,
+    backgroundColor: '#606c38',
+    borderRadius: 8,
+    color: '#fefae0',
+    padding: 16,
+    fontSize: 16,
+  },
+  buttonText: {
+    color: '#fefae0',
+    fontSize: 18,
+    textTransform: 'uppercase',
+    fontWeight: 'bold',
+  },
+  button: {
+    backgroundColor: '#bc6c25',
+    borderRadius: 8,
+    padding: 8,
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection: 'row',
+    gap: 8,
+  },
+  icon: {
+    color: '#fefae0',
+  },
+})
